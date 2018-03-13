dddeb8b86a022b2b1094f423a0fbb0e9dff75f53
guodong <eilyyy@outlook.com>
2018-03-13 10:55:30 +0800

    init project

--- DotNetCoreFileServer/Startup.cs
+++ DotNetCoreFileServer/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace DotNetCoreFileServer
 {
@@ -15,6 +17,7 @@ namespace DotNetCoreFileServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,14 @@ namespace DotNetCoreFileServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            var fsOption = new FileServerOptions
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files")),
+                RequestPath = "/files",
+                EnableDirectoryBrowsing = true,
+                StaticFileOptions = { ServeUnknownFileTypes = true, DefaultContentType = "application/x-msdownload" }
+            };
+            app.UseFileServer(true);
         }
     }
 }
